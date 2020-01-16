406f8536a2007e584d091ffa77b89cb8ae6fd378
Sisi <dqgsh1055@gmail.com>
2020-01-16 16:49:17 +0900

    user update history 기능 테스트코드 작성

--- src/test/java/com/example/aop/AopApplicationTests.java
+++ src/test/java/com/example/aop/AopApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.aop;
 
+import com.example.aop.domain.History;
+import com.example.aop.domain.HistoryRepository;
+import com.example.aop.domain.User;
 import com.example.aop.service.BoardService;
 import com.example.aop.service.UserService;
 import org.junit.jupiter.api.Test;
@@ -7,6 +10,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,6 +25,9 @@ class AopApplicationTests {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private HistoryRepository historyRepository;
+
     @Test
     void contextLoads() {
     }
@@ -32,6 +40,21 @@ class AopApplicationTests {
     @Test
     public void findUsers() throws Exception{
         assertThat(userService.getUsers().size()).isEqualTo(100L);
+    }
+
+    @Test
+    public void updateUsers() throws Exception{
+        List<User> users = userService.getUsers();
+        for(int i=0;i<5;i++){
+            User user = users.get(i);
+            user.setEmail("sisi@sisi.com");
+            userService.update(user);
+        }
+
+        List<History> histories = historyRepository.findAll();
+        assertThat(histories.size()).isEqualTo(5);
+        assertThat(histories.get(0).getUserIdx()).isEqualTo(1L);
+        assertThat(histories.get(1).getUserIdx()).isEqualTo(2L);
     }
 
 }
